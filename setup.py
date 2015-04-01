d408e45ebf17e380626b46733440a8bf9fd390d7
Michael Persson <michael.ake.persson@gmail.com>
2015-04-01 17:11:36 +0200

    Updated to using dark gray for skipping

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ CLASSIFIERS = [
 
 setup(
     name             = 'docker-build-ami',
-    version          = '0.3.5',
+    version          = '0.3.7',
 
     description      = 'Build Amazon EC2 AMI image using a Dockerfile',
     long_description = open("README.rst").read(),
